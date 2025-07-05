79f0feee25660e3a2741f0de3d8461386ec52cac
Sagar-Patel-2005 <sagarpatel24by7@gmail.com>
2025-07-05 22:20:40 +0530

    Full Flutter project upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:expense_tracker_app/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8,),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(fontWeight: FontWeight.bold, color: kColorScheme.onSecondaryContainer, fontSize: 16,),
+        )
+      ),
+      home: const Expenses(),
+    ),
+  );
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,103 @@
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        tital: "Pizza",
+        amount: 500,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        tital: "Ahmedabad",
+        amount: 45,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        tital: "Movie",
+        amount: 270,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        tital: "New Laptop",
+        amount: 45000,
+        date: DateTime.now(),
+        category: Category.work),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) {
+        return NewExpense(onAddExpense: _addExpense,);
+      },
+    );
+  }
+
+  void _addExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense){
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense Deleted."),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = Center(child: Text("No Expenses Found. Start Adding Some!"));
+
+    if(_registeredExpenses.isNotEmpty){
+      mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add))
+        ],
+        title: const Text("ExpenseTracker"),
+        // backgroundColor: Color.fromARGB(25, 25, 25, 25),
+      ),
+      body: Column(
+        children: [
+          const Text("Chart...coming soon"),
+          Expanded(
+            child: mainContent,
+          ),
+        ],
+      ),
+    );
+  }
+}
